f12f84358b62a3d003836f91e1c531a030256ac0
Adeline <adeline.sire@gmail.com>
2021-10-07 12:36:08 +0200

    Backend : Adds server, task model and task controller

---
+++ controllers/task.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const router = express.Router();
+
+const { Task } = require('../models');
+
+const createTask = (req, res) => {
+	const newTask = req.body;
+	const task = new Task({
+		title: newTask.title,
+		description: newTask.description,
+		location: 'tasksList',
+		notDoneCount: 0,
+	});
+	console.log('task.notDoneCount: ', task.notDoneCount);
+
+	task.save((err, task) => {
+		if (err) {
+			res.json({
+				success: false,
+				message: err.toString(),
+			});
+			return;
+		}
+		res.json({
+			success: true,
+			data: task,
+		});
+	});
+};
+
+const readTasks = (req, res) => {
+	Task.find({}, (err, tasks) => {
+		if (err !== null) {
+			res.json({
+				success: false,
+				message: err.toString(),
+			});
+			return;
+		}
+		res.json({
+			success: true,
+			data: tasks,
+		});
+	});
+};
+
+const deleteTask = (req, res) => {
+	console.log('req: ', req);
+	const taskId = req.params.id;
+
+	Task.deleteOne({ _id: taskId }, (err, deletedTask) => {
+		if (err) {
+			res.json({
+				success: false,
+				message: err.toString(),
+			});
+			return;
+		}
+		if (deletedTask.deletedCount === 0) {
+			res.json({
+				success: false,
+				message: 'The task has not been deleted',
+			});
+		}
+		res.json({
+			success: true,
+			data: {
+				isDeleted: true,
+			},
+			message: 'The task has been deleted',
+		});
+	});
+};
+
+const updateTask = (req, res) => {
+	const taskId = req.body._id;
+	const newValues = { ...req.body };
+
+	Task.updateOne({ _id: taskId }, newValues, (err, updatedTask) => {
+		if (err !== null) {
+			res.json({
+				success: false,
+				message: err.toString(),
+			});
+			return;
+		}
+
+		if (updatedTask.nModified === 0) {
+			res.json({
+				success: false,
+				message: `The task hasn't been updated.`,
+			});
+			return;
+		}
+
+		res.json({
+			success: true,
+			data: updatedTask,
+			message: `The task with id ${taskId} has been successfully updated`,
+		});
+	});
+};
+
+router.route('/').post(createTask);
+router.route('/').get(readTasks);
+router.route('/:id').post(deleteTask);
+router.route('/:id').put(updateTask);
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+
+// App
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// BD connection
+const mongoose = require('mongoose');
+const dbConfig = require('./dbConfig');
+const MONGODB_URI =
+	process.env.MONGODB_URI ||
+	`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`;
+mongoose
+	.connect(MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('Successfully connected to MongoDB.');
+	})
+	.catch((err) => {
+		console.error('Connection error', err);
+		process.exit();
+	});
+
+// Controllers
+const { task } = require('./controllers');
+
+// Routes
+app.use('/task', task);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
+});
